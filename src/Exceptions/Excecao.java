143e1fef5266fd3518cdeb1c40c7ec3106051562
Andreza <andrezasilva744@gmail.com>
2023-05-09 20:58:48 -0300

    Aprendendo sobre exceptions

---
+++ src/Exceptions/Excecao.java
@@ -0,0 +1,22 @@
+package Exceptions;
+// https://www.youtube.com/watch?v=ld2C4GcAtsg&ab_channel=LoianeGroner
+public class Excecao {
+    public static void main(String[] args) {
+
+        try{
+            int [] vetor = new int [4];
+
+            System.out.println("Antes da exception");
+
+            vetor[4] = 1;
+
+            System.out.println("Este texto não será impresso");
+        } catch(ArrayIndexOutOfBoundsException exception){
+            System.out.println("Exceção ao acessar um indice do vetor que não existe");
+        }
+
+        System.out.println("Este texto será impresso após a expection");
+
+
+    }
+}
